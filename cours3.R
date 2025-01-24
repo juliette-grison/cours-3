2913db9da58cbc5e998701db4c4be29c529bfb40
Juliette Grison <juliette.grison@gmail.com>
2025-01-24 16:10:25 +0100

    Question 2

--- cours3.R
+++ cours3.R
@@ -1,1 +1,9 @@
+# Question 2
 data_exercice <- read_delim("data/elus-conseillers-municipaux-cm.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+data_exercice <- read.table("C:/Users/julie/Documents/semestre 2/R avancé et GitHub/cours_r_semaine_3/data/elus-conseillers-municipaux-cm.csv", header = TRUE, sep = ";", quote = "")
+
+df_Nantes <- subset(data_exercice, Libellé.de.la.commune == "Nantes")
+df_Faverelles <- subset(data_exercice, Libellé.de.la.commune == "Faverelles")
+df_Loire_Atlantique <- subset(data_exercice, Libellé.du.département == "Loire-Atlantique")
+df_Gers <- subset(data_exercice, Libellé.du.département == "Gers")
+# 
