cdb1b93bbb1a8d7d09ec0d5c22d14240dc70f588
Juliette Grison <juliette.grison@gmail.com>
2025-01-24 16:42:03 +0100

    Question 4

--- cours3.R
+++ cours3.R
@@ -7,16 +7,21 @@ df_Loire_Atlantique <- subset(data_exercice, Libellé.du.département == "Loire-
 df_Gers <- subset(data_exercice, Libellé.du.département == "Gers")
 
 # Question 3
-library(dplyr)
-compter_nombre_d_elus <- function(df) {
+
+validate_schema <- function(df) {
   schema <- c("Code.du.département", "Libellé.du.département", "Code.de.la.collectivité.à.statut.particulier", 
               "Libellé.de.la.collectivité.à.statut.particulier", "Code.de.la.commune", 
               "Libellé.de.la.commune", "Nom.de.l.élu", "Prénom.de.l.élu", 
               "Code.sexe", "Date.de.naissance", "Code.de.la.catégorie.socio.professionnelle", 
               "Libellé.de.la.catégorie.socio.professionnelle", "Date.de.début.du.mandat", 
-              "Libellé.de.la.fonction", "Date.de.début.de.la.fonction", "Code.nationalité" # trouvé avec dput(colnames(data_exercice))
+              "Libellé.de.la.fonction", "Date.de.début.de.la.fonction", "Code.nationalité"
   )
   stopifnot(identical(colnames(df), schema))
+}
+
+library(dplyr)
+compter_nombre_d_elus <- function(df) {
+  validate_schema(df)
   
   df |>
     select(Nom.de.l.élu, Prénom.de.l.élu, Date.de.naissance) |>
@@ -26,6 +31,12 @@ compter_nombre_d_elus <- function(df) {
 
 sapply(list(df_Nantes, df_Faverelles, df_Loire_Atlantique, df_Gers), compter_nombre_d_elus)
 
-compter_nombre_d_elus(df_Nantes |>
-                        select(-Code.du.département)
-                      )
+# Question 4
+library(stringr)
+compter_nombre_d_adjoints <- function(df) {
+  validate_schema(df)
+  
+  sum(str_detect(df$Libellé.de.la.fonction, "adjoint"))
+}
+
+sapply(list(df_Nantes, df_Faverelles, df_Loire_Atlantique, df_Gers), compter_nombre_d_adjoints)
